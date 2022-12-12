79975b323ea84de8605be74384a84ec0c0c1e688
Harshit Joshi <joshiharshit1231@gmail.com>
2022-12-12 13:26:28 +0530

    add div.java

---
+++ div.java
@@ -0,0 +1,10 @@
+public class Divide{
+
+    public static void main(String[]args){
+        int a = 60;
+        int b = 6;
+
+        System.out.println("The division is",
+        + (a/b);
+    }
+}
